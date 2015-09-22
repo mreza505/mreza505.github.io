318d4b14c67514da55ee4e45538cd7d971b1c57f
Nemanja Lazic <nemanja.lazic87@gmail.com>
2015-09-22 20:30:07 +0200

    show username

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
         get: 'tagged',
         tagName: 'hellomreza',
         resolution: 'low_resolution',
-        template: '<div class="col-sm-6 col-md-4"><div class="thumbnail"><a href="{{link}}"><img src="{{image}}" /></a><div class="caption"><p>+{{likes}}</p></div></div></div>',
+        template: '<div class="col-sm-6 col-md-4"><div class="thumbnail"><a href="{{link}}"><img src="{{image}}" /></a><div class="caption"><p>{{model.user.username}}</p></div></div></div>',
         clientId: '43330511fd4b4dbf839cfaf10c73c2c5'
     });
     feed.run();
