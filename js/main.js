b8d4141d2cd4112db228e1d33c0bccce37754c39
lazicnemanja <nemanja.lazic87@gmail.com>
2015-09-24 18:53:04 +0200

    responsive header

--- js/main.js
+++ js/main.js
@@ -8,3 +8,35 @@ $( document ).ready(function() {
     });
     feed.run();
 }); 
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+
+            //ADD REMOVE PADDING CLASS ON SCROLL
+            $(window).scroll(function () {
+                if ($(".navbar").offset().top >50) {
+                    $(".navbar-fixed-top").addClass("navbar-pad-original");
+                } else {
+                    $(".navbar-fixed-top").removeClass("navbar-pad-original");
+                }
+            });
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+    }
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+
